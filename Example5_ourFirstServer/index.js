3084b3964db2674398b2fbadaab79fad66597ef3
Matan Shtepel <46758065+GnarlyMshtep@users.noreply.github.com>
2021-04-02 14:02:58 -0700

    Example 5, incomplete

--- Example5_ourFirstServer/index.js
+++ Example5_ourFirstServer/index.js
@@ -2,6 +2,22 @@ const express = require('express');
 const app = express();
 
 app.listen('3000');
+app.use(express.json())
+//app.use(express.static('public'))
 /*
 
 */
+
+app.post('/api/postSomething', (req, res) => {
+
+    res.json({
+        youSent: req
+    });
+});
+
+app.get('/api', (req, res) => {
+
+    res.json({
+        youAre: 'cool'
+    });
+});
